1e71e570135f3c3f93d3697f0237b64e13487303
SHLim1392 <152074011+SHLim1392@users.noreply.github.com>
2023-11-26 22:40:11 +0900

    [level unrated] Title: 공배수, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181936. 공배수/공배수.py
+++ 프로그래머스/unrated/181936. 공배수/공배수.py
@@ -1,5 +1,3 @@
 def solution(number, n, m):
-    if number % m == 0 and number % n==0:
-        return 1
-    else :
-        return 0
+    return 1 if number % m == 0 and number % n==0 else 0
+       
