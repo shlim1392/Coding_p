9ecb80ef1c2cecc68b28deb9f78f8a3bb643dc00
shlim1392 <152074011+shlim1392@users.noreply.github.com>
2023-12-05 19:00:11 +0900

    Update 조건 문자열.py

--- 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.py
+++ 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.py
@@ -3,3 +3,7 @@ def solution(ineq, eq, n, m):
                 (ineq == ">" and eq == "=" and n >= m) or \
                 (ineq == "<" and eq == "!" and n < m) or \
                 (ineq == ">" and eq == "!" and n > m) else 0
+
+# eval 문자열로 표현된 파이썬 표현식을 인수로 받아 평가하고 결과를 반환
+def solution(ineq, eq, n, m):
+    return int(eval(str(n)+ineq+eq.replace('!', '')+str(m)))
