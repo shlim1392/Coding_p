0ad19c45e2a3fbe6866b08dc661a08494a88f99c
shlim1392 <152074011+shlim1392@users.noreply.github.com>
2023-12-11 09:00:00 +0900

    Update 이어 붙인 수.py

--- 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.py
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.py
@@ -7,3 +7,10 @@ def solution(num_list):
         else:
             odd += str(x)
     return int(even) + int(odd)
+
+def solution(num_list):
+    even=int(''.join([str(i) for i in num_list if i%2==0]))
+    odd=int(''.join([str(i) for i in num_list if not i%2==0]))
+    return even+odd
+
+# join 함수는 리스트의 각 요소를 하나의 문자열로 합치는 역할
