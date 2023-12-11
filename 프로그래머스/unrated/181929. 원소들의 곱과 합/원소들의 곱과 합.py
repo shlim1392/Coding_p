1cb40ac1e75b4d1ae26ba886c57cdf14b09d6f12
shlim1392 <152074011+shlim1392@users.noreply.github.com>
2023-12-11 09:14:41 +0900

    Update 원소들의 곱과 합.py

--- 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.py
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.py
@@ -3,3 +3,7 @@ def solution(num_list):
     a = reduce(lambda x, y: x * y, num_list, 1)
     b = sum(num_list)**2
     return  int(a < b) 
+
+# reduce(...)
+    # reduce(function, iterable[, initial]) -> value
+
