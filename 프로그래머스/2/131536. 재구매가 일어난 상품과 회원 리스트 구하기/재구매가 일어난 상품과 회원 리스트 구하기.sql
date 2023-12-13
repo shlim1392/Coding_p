e5af6b04a0f939e2282a43fe780fa5622256e81b
shlim1392 <152074011+shlim1392@users.noreply.github.com>
2023-12-13 21:32:44 +0900

    Update 재구매가 일어난 상품과 회원 리스트 구하기.sql

--- 프로그래머스/2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/재구매가 일어난 상품과 회원 리스트 구하기.sql
+++ 프로그래머스/2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/재구매가 일어난 상품과 회원 리스트 구하기.sql
@@ -4,3 +4,8 @@ FROM ONLINE_SALE
 GROUP BY USER_ID, PRODUCT_ID
 HAVING COUNT(*) > 1
 ORDER BY USER_ID, PRODUCT_ID DESC;
+
+'''
+이 쿼리는 ONLINE_SALE 테이블에서 USER_ID와 PRODUCT_ID로 그룹을 지어 중복된 조합을 찾은 후,
+HAVING COUNT(*) > 1 조건을 통해 해당 그룹이 두 번 이상 등장한 경우만 선택합니다. 
+'''
