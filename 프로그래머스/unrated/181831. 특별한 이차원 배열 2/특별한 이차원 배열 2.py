e0d7275a777a604d1db5c9c05be2e6c1e682f7da
shlim1392 <152074011+shlim1392@users.noreply.github.com>
2023-12-26 09:42:48 +0900

    [level unrated] Title: 특별한 이차원 배열 2, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.py
@@ -0,0 +1,9 @@
+def solution(arr):
+    n = len(arr)  
+
+    for i in range(n):
+        for j in range(i+1, n):  
+            if arr[i][j] != arr[j][i]:
+                return 0  
+
+    return 1
