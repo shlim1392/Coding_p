bd57fb21d620108952f2639161cb0e07e9b53252
shlim1392 <152074011+shlim1392@users.noreply.github.com>
2024-05-09 07:01:55 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ Python3/프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -0,0 +1,9 @@
+from math import floor
+def solution(price):
+    if price >= 100000 and price < 300000 :
+        price -= price * 0.05
+    elif price >= 300000 and price < 500000:
+        price -= price * 0.1
+    elif price >= 500000:
+        price -= price * 0.2
+    return floor(price)
