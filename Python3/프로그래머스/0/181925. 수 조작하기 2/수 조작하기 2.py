fcd1436f1f594cc144b808b7150886eb6b641b26
shlim1392 <152074011+shlim1392@users.noreply.github.com>
2024-05-13 10:39:52 +0900

    [level 0] Title: 수 조작하기 2, Time: 12.84 ms, Memory: 12.9 MB -BaekjoonHub

---
+++ Python3/프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
@@ -0,0 +1,18 @@
+def solution(numLog):
+    result = ""
+
+    for i in range(1, len(numLog)):
+        diff = numLog[i] - numLog[i-1] 
+        
+        if diff == 1:
+            result += "w"  
+        elif diff == -1:
+            result += "s"  
+        elif diff == 10:
+            result += "d"  
+        elif diff == -10:
+            result += "a" 
+        else:
+            return "Error: Undefined operation detected."
+    return result
+
