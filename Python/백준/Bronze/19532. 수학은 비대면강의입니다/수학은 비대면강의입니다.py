175edad8c160fb9436e6816c3f8aef20acbefc97
shlim1392 <152074011+shlim1392@users.noreply.github.com>
2024-06-04 10:06:47 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 352 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
@@ -0,0 +1,9 @@
+def solution(a, b, c, d, e, f):
+    for x in range(-999, 1000):
+        for y in range(-999, 1000):
+            if a * x + b * y == c and d * x + e * y == f:
+                return x, y
+
+a, b, c, d, e, f = map(int, input().split())
+x, y = solution(a,b,c,d,e,f)
+print(x, y)
