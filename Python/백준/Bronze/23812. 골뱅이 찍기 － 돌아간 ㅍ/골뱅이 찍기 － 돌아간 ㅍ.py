cd79eb46b6b0a389fda6b5ffd93651f7e1337c45
shlim1392 <152074011+shlim1392@users.noreply.github.com>
2024-06-24 07:17:48 +0900

    [Bronze III] Title: 골뱅이 찍기 - 돌아간 ㅍ, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Bronze/23812. 골뱅이 찍기 － 돌아간 ㅍ/골뱅이 찍기 － 돌아간 ㅍ.py
@@ -0,0 +1,22 @@
+n = int(input())
+a = '@'     
+b = "   "
+for _ in range(n):          
+    print((a*n)+(b*n)+(a*n))      
+    
+for _ in range(n):     
+    print(a*5*n)      
+
+for _ in range(n):     
+    print((a*n)+(b*n)+(a*n))
+    
+for _ in range(n):     
+    print(a*5*n)  
+
+for _ in range(n):     
+    print((a*n)+(b*n)+(a*n))
+
+
+    
+    
+    
