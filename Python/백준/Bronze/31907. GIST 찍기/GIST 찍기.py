46c55d776c805ca3832bc40b18d2ae980bace780
shlim1392 <152074011+shlim1392@users.noreply.github.com>
2024-06-24 07:31:08 +0900

    [Bronze III] Title: GIST 찍기, Time: 68 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

---
+++ Python/백준/Bronze/31907. GIST 찍기/GIST 찍기.py
@@ -0,0 +1,13 @@
+n = int(input())
+a = "..."
+b = ".."
+
+for _ in range(n):
+    print(("G"*n)+(a*n))
+    
+for _ in range(n):
+    print(("."*n)+("I"*n)+("."*n)+("T"*n))
+
+for _ in range(n):
+    print((b*n)+("S"*n)+("."*n))
+    
