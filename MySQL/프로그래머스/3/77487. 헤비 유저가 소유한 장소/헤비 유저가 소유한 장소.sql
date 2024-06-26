6364afbc41b792854d7186044cc005061c985b73
shlim1392 <152074011+shlim1392@users.noreply.github.com>
2024-06-27 08:57:34 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ MySQL/프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -0,0 +1,15 @@
+WITH H_USER 
+    AS 
+    (
+        SELECT HOST_ID
+        FROM PLACES
+        GROUP BY HOST_ID
+        HAVING COUNT(ID) >= 2
+    )
+
+SELECT ID,
+       NAME,
+       HOST_ID
+FROM PLACES
+WHERE HOST_ID IN (SELECT HOST_ID FROM H_USER)
+ORDER BY ID;
