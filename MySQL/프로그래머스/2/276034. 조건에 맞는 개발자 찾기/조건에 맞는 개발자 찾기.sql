6e3f21f3aa3424a3f8f238235e4e665d3641dd1c
shlim1392 <152074011+shlim1392@users.noreply.github.com>
2024-07-04 08:24:12 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ MySQL/프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -0,0 +1,14 @@
+SELECT 
+    D.ID, 
+    D.EMAIL, 
+    D.FIRST_NAME, 
+    D.LAST_NAME 
+FROM 
+    DEVELOPERS D
+WHERE EXISTS (
+    SELECT 1
+    FROM SKILLCODES S
+    WHERE (D.SKILL_CODE & S.CODE) != 0 
+    AND (S.NAME = 'Python' OR S.NAME = 'C#')
+)
+ORDER BY D.ID;
