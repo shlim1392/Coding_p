458796768342d70219a0ef6969f56d3662839d72
shlim1392 <152074011+shlim1392@users.noreply.github.com>
2024-07-03 10:29:24 +0900

    [level 0] Title: 편지, Time: 0.03 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120898. 편지/편지.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public int solution(String message) {
+        int charWidth = 2;
+        int messageLength = message.length();
+        int totalWidth = messageLength * charWidth;
+        
+        return totalWidth;
+    }
+}
