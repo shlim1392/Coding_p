1f9e94570f1dc812ea139bbab6fe502b86e47704
shlim1392 <152074011+shlim1392@users.noreply.github.com>
2024-07-18 10:10:51 +0900

    [Silver III] Title: 1로 만들기, Time: 352 ms, Memory: 38932 KB -BaekjoonHub

---
+++ Python/백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -0,0 +1,15 @@
+def solution(n):
+    dp = [0] * (n + 1)
+    
+    for i in range(2, n + 1):
+        dp[i] = dp[i - 1] + 1
+        
+        if i % 2 == 0:
+            dp[i] = min(dp[i], dp[i // 2] + 1)
+        if i % 3 == 0:
+            dp[i] = min(dp[i], dp[i // 3] + 1)
+    return dp[n]
+
+
+n = int(input())
+print(solution(n))
