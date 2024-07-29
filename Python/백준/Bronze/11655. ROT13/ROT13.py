59af28afd6f54a5aaaa2c104e2cca567c675e4da
shlim1392 <152074011+shlim1392@users.noreply.github.com>
2024-07-30 08:13:57 +0900

    [Bronze I] Title: ROT13, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Bronze/11655. ROT13/ROT13.py
@@ -0,0 +1,15 @@
+def rot13(text):
+    result = []
+
+    for char in text:
+        if 'a' <= char <= 'z':
+            result.append(chr((ord(char) - ord('a') + 13) % 26 + ord('a')))
+        elif 'A' <= char <= 'Z':
+            result.append(chr((ord(char) - ord('A') + 13) % 26 + ord('A')))
+        else:
+            result.append(char)
+
+    return ''.join(result)
+
+input_text = input()
+print(rot13(input_text))
