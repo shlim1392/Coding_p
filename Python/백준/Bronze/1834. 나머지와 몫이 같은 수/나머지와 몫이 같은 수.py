6cb8870cd03499e7d36ac9ffe1c3f6f6898b0fbe
shlim1392 <152074011+shlim1392@users.noreply.github.com>
2024-08-02 07:39:09 +0900

    [Bronze I] Title: 나머지와 몫이 같은 수, Time: 224 ms, Memory: 31120 KB -BaekjoonHub

--- Python/백준/Bronze/1834. 나머지와 몫이 같은 수/나머지와 몫이 같은 수.py
+++ Python/백준/Bronze/1834. 나머지와 몫이 같은 수/나머지와 몫이 같은 수.py
@@ -1,5 +1,9 @@
-N = int(input())
-res = 0
-for i in range(1,N):
-    res += N*i + i
-print(res)
+def sum_of_numbers(N):
+    total_sum = 0
+    for k in range(1, N):
+        total_sum += k * (N + 1)
+    return total_sum
+
+N = int(input().strip())
+
+print(sum_of_numbers(N))
