cf6155127d2cf8b990ef7252c1df06fcb3ac58bd
shlim1392 <152074011+shlim1392@users.noreply.github.com>
2024-08-16 06:45:04 +0900

    [Bronze V] Title: 긴자리 계산, Time: 28 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Bronze/2338. 긴자리 계산/긴자리 계산.py
@@ -0,0 +1,13 @@
+def solution():
+    A = int(input())  
+    B = int(input())  
+
+    sum_result = A + B
+    sub_result = A - B
+    mul_result = A * B
+
+    print(sum_result)
+    print(sub_result)
+    print(mul_result)
+
+solution()
