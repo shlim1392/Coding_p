7227bc5689a855ab137ac6e477138e270d1d5145
shlim1392 <152074011+shlim1392@users.noreply.github.com>
2024-08-16 06:47:43 +0900

    [Silver V] Title: 뒤집기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Silver/1439. 뒤집기/뒤집기.py
@@ -0,0 +1,23 @@
+def solution():
+    S = input().strip()
+
+    count_0 = 0
+    count_1 = 0
+    
+    if S[0] == '0':
+        count_0 += 1
+    else:
+        count_1 += 1
+
+    for i in range(1, len(S)):
+        if S[i] != S[i - 1]:
+            if S[i] == '0':
+                count_0 += 1
+            else:
+                count_1 += 1
+
+
+    print(min(count_0, count_1))
+
+
+solution()
