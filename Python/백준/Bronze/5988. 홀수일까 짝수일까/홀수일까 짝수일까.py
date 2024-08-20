5c15709fc3a843e3af12d266d86f1c9cbb2f0c77
shlim1392 <152074011+shlim1392@users.noreply.github.com>
2024-08-21 07:42:53 +0900

    [Bronze III] Title: 홀수일까 짝수일까, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Bronze/5988. 홀수일까 짝수일까/홀수일까 짝수일까.py
@@ -0,0 +1,11 @@
+def solution():
+    n = int(input())
+    for _ in range(n):
+        a = int(input())
+        if a % 2 == 0:
+            print("even")
+        else :
+            print("odd")
+            
+            
+solution()
